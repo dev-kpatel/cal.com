36ea2e11e82d88ad478b5b366777db3b681ce01c
Krunal Patel <ikrunal83@gmail.com>
2026-03-24 18:00:55 +0530

    Update constants.ts

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -79,7 +79,7 @@ export const MAX_EVENT_DURATION_MINUTES = 1440;
 /** Minimum duration allowed for an event in minutes */
 export const MIN_EVENT_DURATION_MINUTES = 1;
 
-export const HOSTED_CAL_FEATURES = process.env.NEXT_PUBLIC_HOSTED_CAL_FEATURES || !IS_SELF_HOSTED;
+export const HOSTED_CAL_FEATURES = true;
 
 export const PUBLIC_QUERY_RESERVATION_INTERVAL_SECONDS =
   parseInt(process.env.NEXT_PUBLIC_QUERY_RESERVATION_INTERVAL_SECONDS ?? "", 10) || 30;
@@ -127,10 +127,9 @@ export const IS_STRIPE_ENABLED = !!(
   process.env.STRIPE_PRIVATE_KEY
 );
 /** This has correct value only server side. When you want to use client side, go for IS_TEAM_BILLING_ENABLED_CLIENT. I think we should use the _CLIENT one only everywhere so that it works reliably everywhere on client as well as server  */
-export const IS_TEAM_BILLING_ENABLED = !!(IS_STRIPE_ENABLED && HOSTED_CAL_FEATURES);
+export const IS_TEAM_BILLING_ENABLED = true;
 
-export const IS_TEAM_BILLING_ENABLED_CLIENT =
-  !!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY && HOSTED_CAL_FEATURES;
+export const IS_TEAM_BILLING_ENABLED_CLIENT = true;
 
 export const FULL_NAME_LENGTH_MAX_LIMIT = 50;
 export const API_NAME_LENGTH_MAX_LIMIT = 80;
